29609b91e2a35775afc2448d5b70c0fa72dade0b
lifeng <26571864@qq.com>
2013-03-01 23:13:51 +0800

    modified:   build.xml 	deleted:    build/classes/dom4j-1.6.1.jar 	modified:   codeHelper.py 	deleted:    lib/dom4j-1.6.1.jar

--- codeHelper.py
+++ codeHelper.py
@@ -13,6 +13,10 @@ import sys
 if __name__=='__main__':
     print sys.argv[0]
     print sys.argv[1]
-    command_output=os.system('scp build/bin/sqoop_export_job.jar   hadoop@jobtracker:/home/hadoop/sqoop/sqoop_export_job.jar')
-    if command_output==0:
-        print "deploy file is ok! "
+    if sys.argv[1]=='jar':
+        command_output=os.system('ant jar')
+    if sys.argv[1]=='deploy':
+        command_output=os.system('ant jar') 
+        command_output=os.system('scp build/bin/sqoop_export_job.jar   hadoop@jobtracker:/home/hadoop/sqoop/sqoop_export_job.jar')
+        if command_output==0:
+            print "deploy file is ok! "
