bfdb3a1fc4ff5ed2febc627a57af0ff4d5a1fa76
lifeng <26571864@qq.com>
2013-03-01 22:57:17 +0800

    modified:   codeHelper.py

--- codeHelper.py
+++ codeHelper.py
@@ -2,18 +2,17 @@
 # -*- coding: utf-8 -*-
 # filename: pexpect_test.py
 '''
-Created on 2010-7-2
-@author: forever
+Created on 2013-03-01
+@author: lifeng
 '''
 
 import pexpect
-
+import os
+import sys
 
 if __name__=='__main__':
-    user = 'hadoop'    
-    ip = '10.71.84.233'
-    passwd = 'wyyhzc790204'
-    print user
-    child= pexpect.spawn('scp build/bin/sqoop_export_job_201302252250.jar   hadoop@jobtracker:/home/hadoop/sqoop/sqoop_export_job_201302252250_02.jar')
-    print str(child)
-    print "deploy file is ok! "
+    print sys.argv[0]
+    print sys.argv[1]
+    command_output=os.system('scp build/bin/sqoop_export_job.jar   hadoop@jobtracker:/home/hadoop/sqoop/sqoop_export_job.jar')
+    if command_output==0:
+        print "deploy file is ok! "
